5366847e74f0db454c1a2862e046bce6efabc163
Sachin Prajapati <53299703+sachinprajapati8604@users.noreply.github.com>
2021-07-17 12:51:38 +0530

    written js for enter key to add

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
+//calling Add function by enter key  
+var input = document.getElementById("myinput");
+input.addEventListener("keypress", function(event) {
+  if (event.key === 'Enter') {
+   event.preventDefault();
+   document.getElementById("addbtn").click();
+  }
+});
+
+//or we can call like this by using key code  (enter key code is 13 )
+
+// var input = document.getElementById("myinput");
+// input.addEventListener("keyup", function(event) {
+//   if (event.keyCode === 13) {
+//    event.preventDefault();
+//    document.getElementById("addbtn").click();
+//   }
+// });
+
 function Add() {
     var myinput = document.getElementById('myinput').value;
+  
     if (myinput === "") {
-        alert("Please write something");
+        // alert("Please write something");
+        swal("Input Can't  empty", "Please write something", "warning", {
+            button: "Ok",
+          });
     } else {
         
         var outputbox = document.getElementById('outputbox');
